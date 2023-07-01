21a41b17acecf07235b3a581307ea8fe818113e4
gabrielPedrozaRobotics <137827265+gabrielPedrozaRobotics@users.noreply.github.com>
2023-07-01 08:28:52 -0300

    Add files via upload
    codes with some tweaking needed, but mostly up-to-date

---
+++ mariaFinal_arrumado.ino
@@ -0,0 +1,466 @@
+#include "SoftwareSerial.h"
+#define initial_angle 125
+#define head_pin 7
+#define leftDCPin1 37
+#define rightDCPin1 33
+#define leftDCPin2 39
+#define rightDCPin2 35
+#define leftDCPwm 6
+#define rightDCPwm 11
+// RX: Arduino pin 47, XBee pin DOUT.  TX:  Arduino pin 49, XBee pin DIN
+SoftwareSerial XBee(47, 49);
+#include <Adafruit_NeoPixel.h>
+#include <Servo.h>
+#define K 0x000000//BLACK
+#define B 0x0048ff//BLUE
+#define G 0x008000//GREEN
+#define P 0x9d00d2//PURPLE
+#define R 0xFF0000//RED
+#define O 0xFFA500//ORANGE
+#define S 0xFFC0CB//ROSE
+#define Y 0xFf6400//YELLOW
+#define W 0xFFFFFF//WHITE
+#define PIN 12// Pin of controle
+#define LED_COUNT 66// Number of leds
+//Cria uma instância da Classe Adafruit NeoPixel chamado "leds"
+#define innerLeftShoulder 8
+#define outerLeftShoulder 10
+#define leftElbow 9
+#define innerRightShoulder 2
+#define outerRightShoulder 3
+#define rightElbow 5//CAUTION! verify ports and change if needed
+class articulation{  
+  public:
+    articulation(Servo mmotor, int ini, int(*ffunc)(int x)){ //initializes an articulation
+      _motor = mmotor; //motor
+      _func = ffunc; //function to adjust angles
+      _ang = _func(ini); //angle in which the articulation is
+      move(ini); //sets the initial position
+    }
+    void move(int x, int del){
+      x = _func(x); //adjusts angle
+      if(x > _ang){ //if it needs to increase angle
+        for(; _ang <= x; ++_ang){
+          _motor.write(_ang);
+          delay(del); //moves slowly
+          //Serial.println("subindo" + String(_ang));
+        }
+        _ang=x; //sets the end angle to desired
+      }else{
+        for(; _ang >= x; --_ang){
+          _motor.write(_ang);
+          delay(del);//moves slowly
+          //Serial.println("descendo" + String(_ang));
+        }
+        _ang=x; //sets the end angle to desired
+      }
+    }
+    void move(int x){
+      move(x, 15); //quick move, with standard speed
+    }
+    void brute(int x){
+      _motor.write(x); //write directly to motor
+    }
+  private:
+    int _ang;
+    Servo _motor;
+    int(*_func)(int x);
+};
+Servo mirs, mors, mre, mils, mols, mle; //acronyms for above definitions
+articulation *irs, *ors, *re, *ils, *ols, *le; //articulations
+//functions for adjusting angle 
+Servo head_motor;
+
+int adjustIRS(int x){ /*return (x+30>180)?180:x+30;*/ return x;}
+int adjustORS(int x){ return 180-x;}
+int adjustRE(int x){ return (x>155)?180-155:180-x;}
+int adjustILS(int x){ return 180-x;}
+int adjustOLS(int x){ return 180-x;}
+int adjustLE(int x){ return (x>155)?180-155:180-x;}
+void greet();
+void selfDestroy();
+void calculate();
+void track();
+void receipt();
+void angrier();
+void normal();
+void understand();
+void translate();
+void object();
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+
+//unsigned long time;//Will count the time
+//unsigned long timeout;//timeout
+int brilho[3];//Receives the values of the serial
+//int i=0;//amount of characters
+int posicao=0;
+int flag=0;//Informs that a information by the serial was received
+char temp,cor;//Storages data received by the serial
+void setup() {
+  // put your setup code here, to run once:
+  head_motor.attach(head_pin);
+  head_motor.write(initial_angle); 
+  leds.begin();
+  //leds.setPixelColor(30, B);
+  leds.show();
+  pinMode(13, OUTPUT);
+  //digitalWrite(6, 1);
+  pinMode(leftDCPin1, OUTPUT);
+  pinMode(leftDCPin2, OUTPUT);
+  pinMode(rightDCPin1, OUTPUT);
+  pinMode(rightDCPin2, OUTPUT);
+  pinMode(rightDCPwm, OUTPUT);
+  pinMode(leftDCPwm, OUTPUT);
+  
+  mirs.attach(innerRightShoulder);
+  mors.attach(outerRightShoulder);
+  mre.attach(rightElbow);
+  mils.attach(innerLeftShoulder);
+  mols.attach(outerLeftShoulder);
+  mle.attach(leftElbow);
+  delay(5000);
+  irs = new articulation(mirs, 0, adjustIRS);
+  delay(2000);
+  ors = new articulation(mors, 0, adjustORS);
+  delay(2000);
+  re = new articulation(mre, 0, adjustRE);
+  delay(2000);
+  ils = new articulation(mils, 0, adjustILS);
+  delay(2000);
+  ols = new articulation(mols, 0, adjustOLS);
+  delay(2000);
+  le = new articulation(mle, 0, adjustLE);
+  delay(4000);//initializing motors. plenty of delay to let capacitors stabilize */
+  Serial.begin(115200);
+  delay(200);
+  Serial.setTimeout(5); //usual set-up
+  XBee.begin(9600);
+  leds.setPixelColor(10, R);
+  leds.show();
+  analogWrite(leftDCPwm, 255);
+  analogWrite(rightDCPwm, 255);
+  leftStop();
+  rightStop();
+  Serial.println('z');
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0)
+  {
+    //Serial.println("opa");
+    char i = Serial.read();
+    if(i == 'D'){
+      //makes her angry and sends the little kart
+      selfDestroy();
+    }else if(i == 'G'){
+      //waves goodbye and turns on the leds
+      Serial.println("greeting");
+      greet();
+    }else if(i == 'T'){ //tracking
+      //recognize
+      understand();
+      track();
+      normal();
+    }else if(i == 'O' || i == 'C'){ //ocr or calculate
+      //recognize
+      understand();
+      normal();
+    }else if(i == 'R'){ //receipt
+      //recognize kinda angry
+      receipt();
+    }else if(i == 'A'){
+      //recognize even angrier
+      angrier();
+    }else if(i == 'I'){
+      object();
+    }
+    Serial.flush();
+  }
+}
+void angrier(){
+    for(int i = 0; i <= LED_COUNT; ++i){
+      if(i%3 == 0){
+        leds.setPixelColor(i,R);//turns the leds mutiples of three red
+        delay(10);
+        leds.show();
+      }
+    }
+    for(int x = 0; x <=250;){//makes the go full circle aproximately three times
+      for (int i = 0; i<=LED_COUNT; i++)
+      {
+     
+        leds.setPixelColor(i,Y);//turns the led yellow
+        delay(5);
+        if((i-3)%3==0 || (i-3)%5==0 || (i-3)%7==0){//if the former was red, it returns blue
+        leds.setPixelColor(i-3, R);
+        leds.show();
+        delay(10);
+      }
+       else{//if it wasn't red (was blue) it returns blue
+        leds.setPixelColor(i-3, B);
+        leds.show();
+        delay(10);
+       }
+       if (i%6==0){//if its multiple of six, becomes yellow
+        leds.setPixelColor(i-3, Y);
+        leds.show();
+        delay(10);
+       }
+      x = x+1;
+     }
+    }
+}
+void selfDestroy(){
+  digitalWrite(LED_BUILTIN, HIGH);
+  //XBee.write('H');//orders the secondary robot to leave
+  bool a = true;
+  for(int x = 0; x > -160 && a;){
+    for (int i = 0; i<=LED_COUNT &&a; i++)//makes an yellow circle
+    {
+      leds.setPixelColor(i,Y);//activates the required led
+      delay(20);
+      leds.setPixelColor(i-10, R);
+      leds.show();
+      x++;
+      if(x >= 50*5.8 && a){ XBee.write('D'); a = false;}//orders the secondary robot to leave
+    }
+  }
+  delay(3000);
+  for(byte i = 1; i <= 8; ++i){
+    leftFowards();
+    rightBackwards();
+    delay(150);
+    leftBackwards();
+    rightFowards();
+    delay(150);
+  }
+  Serial.println('r');
+  leftStop();
+  rightStop();
+  understand();
+}
+void greet(){
+  for (int i = 0; i<=LED_COUNT; i++)
+    {
+      leds.setPixelColor(i,P);//Turns the color
+      delay(7);
+      //leds.setPixelColor(i-15, P);
+      leds.show();
+    }
+    delay(100);
+    for(int i = 0; i <= LED_COUNT; ++i){
+      if(i%7 == 0){
+        leds.setPixelColor(i,B);//turns the leds mutiples of seven blue
+        delay(15);
+        leds.show();
+      }
+    }
+    for(int i = 0; i <= LED_COUNT; ++i){
+      if(i%5 == 0){
+        leds.setPixelColor(i,B);//turns the leds mutiples of five blue
+        delay(15);
+        leds.show();
+      }
+    }
+    for(int i = 0; i <= LED_COUNT; ++i){
+      if(i%2 == 0){
+        leds.setPixelColor(i,B);//turns the leds mutiples of 2 blue
+        delay(15);
+        leds.show();
+      }
+    }
+    for(int i = 0; i <= LED_COUNT; ++i){
+      if(i%1 == 0){
+        leds.setPixelColor(i,B);//turns all leds blue
+        delay(15);
+        leds.show();
+      }
+    }
+
+    leftFowards();
+    rightFowards();
+    delay(1000);
+    leftStop();
+    rightStop();
+    Serial.println('r');
+    ors->move(50, 5);
+    re->move(100, 5);
+    irs->move(30, 5);
+    re->move(90, 5);
+    irs->move(180, 5);
+    ors->move(30);
+    re->move(100, 5);
+    delay(1000);
+    re->move(30, 5);
+    re->move(100, 5);
+    delay(1000);
+    irs->move(0, 5);
+    re->move(0, 10);
+    ors->move(0, 10);
+}
+void moveLeft(byte Pin1, byte Pin2, byte velocity){
+  analogWrite(leftDCPwm, velocity);
+  digitalWrite(leftDCPin1, Pin1);
+  digitalWrite(leftDCPin2, Pin2); 
+}
+void leftFowards(byte velocity){
+  moveLeft(1, 0, velocity);
+}
+void leftFowards(){
+  leftFowards(150);
+}
+void leftBackwards(byte velocity){
+  moveLeft(0, 1, velocity);
+}
+void leftBackwards(){
+  leftBackwards(150);
+}
+void leftStop(){
+  moveLeft(0, 0, 255);
+}
+void moveRight(byte Pin1, byte Pin2, byte velocity){
+  analogWrite(rightDCPwm, velocity);
+  digitalWrite(rightDCPin1, Pin1);
+  digitalWrite(rightDCPin2, Pin2); 
+}
+void rightFowards(byte velocity){
+  moveRight(1, 0, velocity);
+}
+void rightFowards(){
+  rightFowards(150);
+}
+void rightBackwards(byte velocity){
+  moveRight(0, 1, velocity);
+}
+void rightBackwards(){
+  rightBackwards(150);
+}
+void rightStop(){
+  moveRight(0, 0, 255);
+}
+void understand(){
+    for(int x = 0; x<=300;){
+      for (int i = 0; i<=LED_COUNT; i++){
+        leds.setPixelColor(i,G);//activates the required led
+        delay(10);
+        leds.setPixelColor(i-15, B);
+        leds.show();
+        x = x+1;
+      }
+   }
+}
+void track(){
+  rightFowards();
+  leftBackwards();
+  delay(500);
+  rightStop();
+  leftStop();
+  int angulo = initial_angle;
+  int lista[6]; int j = 0;
+  long int timee = 0;
+  while(millis() - timee <= 18000){
+    if(Serial.available()){ 
+      ++j;
+      int i = Serial.readString().toInt(); //here, we get our displacement from the PC
+      if(abs(i-10)<=1) i = 10;//if the displacement is too small, we just ignore it
+      lista[j] = -i+10; //since we cannot send negative numbers over serial, we have to convert the displacement to a proper scale
+      /*
+       * j, list[], maxx, minn, and total are all auxiliary variables that help us make our angles consistent
+       * 
+       * ocasionally, due to fluctuations, the Python code will send angles that are not coherent with reality
+       * thus, in order to filter these outliers out, we store 4 successive values in a vector before writing the angle to the motor
+       * we then disregard both the highest and the lowest values. we average the two left out, and only then do we send the angle to the motor
+       * 
+       * notice that this process takes time, which requires us to lower the explicit delay associated with the movement
+       * (since there's a lot of time involved in doing these calculations, waiting for the receipt of four values, etc
+       * that the motor will already utilize in its movement)
+       */
+      if(j == 4){
+        int maxx = 1, minn = 1, total = 0; 
+        for(int k = 1; k <= 4; ++k){
+          if(lista[k] > lista[maxx]) maxx = k;
+          if(lista[k] < lista[minn]) minn = k;
+          total += lista[k];
+        }
+        angulo += 1*(total-lista[maxx]-lista[minn])/2;
+        if(angulo < 0) angulo = 0;
+        else if(angulo > 180) angulo = 180;
+        head_motor.write(angulo);
+        digitalWrite(LED_BUILTIN, 1);
+        j = 0; //of course, it is necessary to reset the index of the list after sending the angle
+        //delay(5);
+        digitalWrite(LED_BUILTIN, 0);
+      }
+    }
+    Serial.flush();//we flush it to be certain that the values read are always up-to-date 
+  }
+  head_motor.write(initial_angle);
+  leftFowards();
+  rightBackwards();
+  delay(500);
+  leftStop();
+  rightStop();
+  Serial.println('t');
+}
+void normal(){
+    for (int i = 0; i<=LED_COUNT; i++)
+    {
+      leds.setPixelColor(i,B);//activates the required led
+      delay(20);
+      leds.setPixelColor(i-10, B);
+      leds.show();
+    }
+}
+void receipt(){
+    for(int i = 0; i <= LED_COUNT; ++i){
+      if(i%5 == 0){
+        leds.setPixelColor(i,R);//turns the leds mutiples of five red
+        delay(15);
+        leds.show();
+      }
+    }
+    for(int i = 0; i <= LED_COUNT; ++i){
+      if(i%7 == 0){
+        leds.setPixelColor(i,R);//turns the leds mutiples of seven red
+        delay(15);
+        leds.show();
+      }
+    }  
+}
+void translate(){
+  //move motors
+}
+void object(){
+  rightFowards();
+  leftBackwards();
+  delay(500);
+  rightStop();
+  leftStop();
+  ors->move(50, 5);
+  re->move(100, 5);
+  irs->move(180, 5);
+  delay(500);
+  re->move(30, 5);
+  int angle = 30;
+  long int timee = millis();
+  while(millis() - timee <= 4000||true){
+    if(Serial.available()){
+      int i = Serial.readString().toInt(); //here, we get our displacement from the PC
+      if(abs(i-10)<=1) i = 10;//if the displacement is too small, we just ignore it
+      angle += i;
+      if(angle < 0) angle = 0;
+      else if(angle > 180) angle = 180;
+      re->move(angle);
+      digitalWrite(LED_BUILTIN, 1);
+      //delay(5);
+      digitalWrite(LED_BUILTIN, 0);
+    }
+    Serial.flush();//we flush it to be certain that the values read are always up-to-date 
+  }
+  leftFowards();
+  rightBackwards();
+  delay(500);
+  leftStop();
+  rightStop();
+}
