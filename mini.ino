fea2b5723392126abb89c0930923d660e4754da9
gabrielPedrozaRobotics <137827265+gabrielPedrozaRobotics@users.noreply.github.com>
2023-07-04 12:53:36 -0300

    Update mini.ino
    pristine xbee

--- mini.ino
+++ mini.ino
@@ -1,3 +1,30 @@
+/*
+  Software serial multiple serial test
+
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+
+ The circuit:
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
+
+ Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts,
+ so only the following can be used for RX:
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+
+ Not all pins on the Leonardo and Micro support change interrupts,
+ so only the following can be used for RX:
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+
+ This example code is in the public domain.
+
+ */
 #include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h>
 #include <Servo.h>
@@ -19,7 +46,7 @@
 #define LED_COUNT 79// Number of leds
 //Cria uma instância da Classe Adafruit NeoPixel chamado "leds"
 Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
-SoftwareSerial XBee(3, 4); // RX, TX
+SoftwareSerial XBee(3, 4); // RX DOUT, TX DIN
 Servo le, ri;
 void fowardsMini()
 {
@@ -69,7 +96,7 @@ void loop() { // run over and over
       //delay(500);
     char c = XBee.read();
     //Serial.println(c);
-    if (c == 'H')
+    if (c == 'D')
     {
       //if it received the character it walks foward
       digitalWrite(LED_BUILTIN, HIGH);
